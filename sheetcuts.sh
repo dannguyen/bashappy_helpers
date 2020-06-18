a3040ce071a11cded6b0e40c71788a2460efb81b
Dan Nguyen <dansonguyen@gmail.com>
2020-06-17 21:03:12 -0500

    allowed xcel/xlibre to accept multiple arguments

--- sheetcuts.sh
+++ sheetcuts.sh
@@ -1,9 +1,32 @@
 function xcel {
-        echo "Opening '$1' in Microsoft Excel..."
-        open -a 'Microsoft Excel' $1
+    # Opens file(s) into Microsoft Excel
+    # Note: requires Microsoft Excel;  only works on macOS
+    #
+    # Example usage:
+    #
+    # $ xcel mybook.xlsx
+    # $ xcel *.xls*
+    # $ xcel *.csv
+    echo "Opening ${#} file(s) in Microsoft Excel:"
+    for fname in "$@"; do
+        printf "\t%s\n" "${fname}"
+        open -a 'Microsoft Excel' "${fname}"
+    done
+
 }
 
-function libxl {
-        echo "Opening '${1}' in LibreOffice, that's praxis"
-        open -a 'LibreOffice' "${1}"
+function xlibre {
+    # Opens file(s) into LibreOffice Calc
+    # Note: requires LibreOffice; only works on macOS
+    #
+    # Example usage:
+    #
+    # $ xlibre mybook.xlsx
+    #
+    # $ xlibre *.csv
+    echo "Opening ${#} file(s) in LibreOffice (that's praxis!)"
+    for fname in "$@"; do
+        printf "\t%s\n" "${fname}"
+        open -a 'LibreOffice' "${fname}"
+    done
 }
