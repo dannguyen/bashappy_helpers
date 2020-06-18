ab0d66efaf99f87ec030698180decae40011d369
Dan Nguyen <dansonguyen@gmail.com>
2020-06-17 21:06:27 -0500

    added more notes to tabname functions

--- tabname.sh
+++ tabname.sh
@@ -1,21 +1,31 @@
-### helper functions to set the tab title
+### helper functions to set the Terminal tab title
+
+# More info:
+# https://superuser.com/questions/105499/change-terminal-title-in-mac-os-x/#131177
 
 
 function tabname {
-  # Set macOS tab title to given arguments, sans directory path
+  # Set macOS Terminal tab title to given arguments
+  #
+  #  Example:
   #   $ tabtitle Hello this is my new tab title
   printf "\033]1;%s\007" "$*"
 }
 
 function tadname {
-    # like tabname() except show grandparent and parent subdir path
-    # WARNING: the subdir component does not auto-update when changing directories
+    # Like tabname() except show grandparent and parent subdir path
     #
     #  Example:
     #    /Users/me/projects/subname $ tabdname hello new tab name
-
     #  Result title:
     #    hello new tab name – ../projects/subname
+
+    #  Note: By default, the parent Terminal reflects the subdirectory of the
+    #   currently active tab, thus making this function kind of redundant?
+    #
+    #  WARNING: the subdir component does not auto-update
+    #    when changing directories
+
     gname=$(basename "$(dirname $(pwd))")
     pname=$(basename "$(pwd)")
     if [ "$gname" = '/' ]
@@ -30,5 +40,3 @@ function tadname {
     tabname "$* – ${subname}"
 }
 
-# More info:
-# https://superuser.com/questions/105499/change-terminal-title-in-mac-os-x/#131177
